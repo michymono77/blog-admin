725717ca19a97903718a8ad5280c4a119deabcbc
Michiharu Ono <michiharuono77@gmail.com>
2021-12-10 17:50:08 +0900

    setup complete. Author controller, layout added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :authors
+  get 'home/index'
   root to: "home#index"
+  scope module: 'authors' do
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
