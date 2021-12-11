7a334123bba8d4049b93ff935db1ab0632b78361
Michiharu Ono <michiharuono77@gmail.com>
2021-12-11 16:57:01 +0900

    element model added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :authors
   get 'home/index'
   root to: "home#index"
   scope module: 'authors' do
-    resources :posts
+    resources :posts do
+      resources :elements
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
