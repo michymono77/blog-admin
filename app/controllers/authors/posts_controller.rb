e3bfe6bf3ae957fc6b4302c5facde8b7978f3c42
Michiharu Ono <michiharuono77@gmail.com>
2021-12-13 12:26:29 +0900

    image submission style changed

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -28,28 +28,20 @@ class PostsController < AuthorsController
   def create
     @post = current_author.posts.build(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to edit_post_path(@post)
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+      redirect_to edit_post_path(@post)
+      else 
+      render :edit
       end
-    end
   end
 
   # DELETE /posts/1 or /posts/1.json
@@ -69,7 +61,7 @@ class PostsController < AuthorsController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description)
+      params.require(:post).permit(:title, :description, :header_image)
     end
 end
 end
