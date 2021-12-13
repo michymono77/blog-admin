e47bb8608d96fd6005382bc5014b84c5cef575eb
Michiharu Ono <michiharuono77@gmail.com>
2021-12-13 18:02:36 +0900

    stimulus reflex working in progress

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_author
+
+    def connect
+      self.current_author = find_verified_author
+    end
+
+    protected
+
+    def find_verified_author
+      if current_author == Author.find(1)
+        current_author
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -40,3 +40,5 @@ document.addEventListener('turbolinks:load', () => {
     element.previousElementSibling.classList.remove('d-none')
   })
 })
+
+import "controllers"
